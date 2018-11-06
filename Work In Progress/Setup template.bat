96cefb7d9a622b004ab7570056b487c79933bf61
MaltronCraft Network <41383245+MaltronCraft@users.noreply.github.com>
2018-11-06 09:30:30 -0700

    Update Setup template.bat

--- Work In Progress/Setup template.bat
+++ Work In Progress/Setup template.bat
@@ -1,2 +1,14 @@
 @echo off
 title Setup
+echo Downloading setup files...
+cd C:/
+md SetupDownloads
+cd SetupDownloads
+bitsadmin /transfer SetupFiles /download /priority high (files url) C:/SetupDownloads/(file name) || goto :FailedToRunBitsadmin
+bitsadmin /transfer SetupFiles /download /priority high (files url) C:/SetupDownloads/(file name) || goto :FailedToRunBitsadmin
+echo Files downloaded!
+echo Installing!
+(copy code placeholder)
+(verify code placeholder)
+(licence code placeholder)
+(T & A code placeholder)
